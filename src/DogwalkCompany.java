8269f6b2f2520f7ed036ccb12103c13a07a31109
margauxk22 <margauxk2@nycstudents.net>
2025-09-26 11:18:48 -0400

    started

---
+++ src/DogwalkCompany.java
@@ -0,0 +1,6 @@
+public class DogwalkCompany {
+    private int [] schedule;
+    public DogwalkCompany() {
+        schedule = new int[24];
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main
+{
+    public static void main(string [] args ) {
+        DogwalkCompany c = new DogwalkCompany();
+        System.out.println(c);
+    }
+
+}
