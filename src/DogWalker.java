be0290c3c3da267f80051e35d2b601bb46d9fc45
margauxk22 <margauxk2@nycstudents.net>
2025-10-14 11:50:12 -0400

    work

---
+++ src/DogWalker.java
@@ -0,0 +1,12 @@
+public class DogWalker {
+
+    private int maxDogs;
+    private DogwalkCompany company;
+
+    public DogWalker(int max, DogwalkCompany comp){
+        maxDogs = max;
+        company = comp;
+    }
+
+
+}
