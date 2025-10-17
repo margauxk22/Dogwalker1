1d9bdb90f61650ebf719d5da66bf5beba93175bd
margauxk22 <margauxk2@nycstudents.net>
2025-10-17 11:18:16 -0400

    Finished.

--- src/DogWalkCompany.java
+++ src/DogWalkCompany.java
@@ -24,26 +24,16 @@ public class DogWalkCompany {
         return hour;
     }
 
-    /**
-     * Returns the number of dogs, always greater than 0, that are available
-     * for a walk during the time specified by hour
-     * Precondition: 0 <= hour <= 23
-     */
+
     public int numAvailableDogs(int hour) {
         return schedule[hour];
     }
 
-    /**
-     * Decreases, by numberDogsWalked, the number of dogs available for a walk
-     * during the time specified by hour
-     * Preconditions: 0 <= hour <= 23
-     * numberDogsWalked > 0
-     */
+
     public void updateDogs(int hour, int numberDogsWalked) {
         schedule[hour] -= numberDogsWalked;
     }
 
 
-/* There may be instance variables, constructors,
-and methods that are not shown. */
+
 }
